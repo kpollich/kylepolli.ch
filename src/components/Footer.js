1002da0c8b6bad06465d4c6646407579e9d35f7f
Kyle Pollich <kpollich1@gmail.com>
2017-11-08 16:55:37 -0500

    Add simple footer + Contentful attribution

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className="footer">
+    <div className="container">
+      <div className="content has-text-centered">
+        <div>Kyle Pollich - Lancaster, PA Full Stack Developer</div>
+        <div style={{ marginTop: '2em' }}>
+          <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
+            <img
+              src="https://images.contentful.com/fo9twyrwpveg/7F5pMEOhJ6Y2WukCa2cYws/398e290725ef2d3b3f0f5a73ae8401d6/PoweredByContentful_DarkBackground.svg"
+              style={{ maxWidth: '100px', width: '100%' }}
+              alt="Powered by Contentful"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 import 'bulma'
 import './index.scss'
@@ -21,6 +22,7 @@ const TemplateWrapper = ({ children }) => (
     />
     <Header />
     <div>{children()}</div>
+    <Footer />
   </div>
 )
 
