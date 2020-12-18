3a2a83bce633ac35914e9910808a8120b6f85bf1
Kyle Pollich <kpollich1@gmail.com>
2020-12-18 15:38:00 -0500

    Improve vertical spacing in markdown content

--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -22,18 +22,16 @@ const UsesPage: React.FunctionComponent<Props> = ({ content }) => {
         thoughts, and try to stay productive. I'm always trying out new apps
         and gadgets, so don't consider this list exhaustive or complete."
       />
-      <section>
+
+      <article className="markdown-content max-w-screen-lg m-auto text-lg">
         <h1>Uses</h1>
         <p>
           These are some of the things I use to write code, organize my
           thoughts, and try to stay productive. I'm always trying out new apps
           and gadgets, so don't consider this list exhaustive or complete.
         </p>
-
-        <article className="markdown-content max-w-screen-lg m-auto text-lg">
-          {mdxContent}
-        </article>
-      </section>
+        {mdxContent}
+      </article>
     </Layout>
   );
 };
