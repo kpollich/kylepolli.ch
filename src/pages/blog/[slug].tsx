38314e80101921d9b82b976d0344488200b52da3
Kyle Pollich <kpollich1@gmail.com>
2021-11-02 09:04:44 -0400

    Remove unused MDX serialize import

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import { NextPage } from 'next';
 import Image from 'next/image';
 import { MDXRemote } from 'next-mdx-remote';
-import { serialize } from 'next-mdx-remote/serialize';
 
 import {
   childVariants,
