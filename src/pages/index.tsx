4e5b9965284aa51fe20782af6011eb8ec22c4cb0
Kyle Pollich <kpollich1@gmail.com>
2022-06-01 08:13:09 -0400

    Update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ const IndexPage: React.FunctionComponent = () => {
             </p>
 
             <p className="mt-4">
-              I'm currently a Senior Software Engineer at{' '}
+              I'm currently a Tech Lead at{' '}
               <a
                 href="https://www.elastic.co/"
                 className="hover:text-cyan-600 underline"
