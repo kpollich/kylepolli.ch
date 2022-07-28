8bfa8663af92a9cda1517dec77b070a5dd630744
Kyle Pollich <kpollich1@gmail.com>
2022-07-28 12:37:41 +0000

    Update 3 files

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -1,6 +1,10 @@
 import { FunctionComponent } from 'react';
 
-const ContentWrapper: FunctionComponent = ({ children }) => {
+const ContentWrapper: FunctionComponent = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}) => {
   return <section className="max-w-screen-lg p-8 m-auto">{children}</section>;
 };
 
